f0838972bfb93088899791c901ceadcd53dfcbc0
Su Zhang <suzhang79@gmail.com>
2023-07-13 20:22:23 +1000

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by 张肃 on 13/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
